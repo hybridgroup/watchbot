798a2b839b16e44ef4da8559caad1b4a4fdd32c3
Javier Cervantes <1.27201@gmail.com>
2014-03-27 15:48:21 -0600

    Removing correct declaration of response

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,10 +1,10 @@
 function fetchMessage() {
-  var response;
   var req = new XMLHttpRequest();
   req.open('GET', "http://192.168.1.15:8080/robots/pebble/devices/pebble/commands/last_message", true);
   req.onload = function(e) {
     if (req.readyState == 4) {
       if(req.status == 200) {
+        var response = JSON.parse(req.responseText);
         var message = response.result.toString();
         Pebble.sendAppMessage({
           "message": message
