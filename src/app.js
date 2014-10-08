e3708206d0e2f337cc25e496b4f77b3a98fdb4e9
Javier Cervantes <1.27201@gmail.com>
2014-10-08 11:12:20 -0500

    Adjusting settings and accelerometer config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 var UI = require('ui');
 var Accel = require('ui/accel');
+var Settings = require('settings');
 
 function getUrlFor(command){
-  var name    = localStorage.name;
-  var host    = localStorage.host;
-  var port    = localStorage.port;
-  var device  = localStorage.device;
+  var name    = Settings.data('name');
+  var host    = Settings.data('host');
+  var port    = Settings.data('port');
+  var device  = Settings.data('device');
 
   return host + ":" + port + "/api/robots/" + name + "/devices/" + device + "/commands/" + command;
 }
@@ -49,14 +50,12 @@ function pollForMessages() {
 pollForMessages();
 
 Pebble.addEventListener("showConfiguration", function() {
+  var html = '<html> <head> <title>watchbot configuration</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> </head> <style> body, textarea, button { font-size: 20px; font-family: monospace; text-align: center; } textarea { resize: none; padding-top: 15px; } button { padding: 15px; } </style> <body> <div data-role="page" id="main"> <div data-role="header" class="jqm-header"> <h1>Configure watchbot</h1> </div> <div data-role="content"> <div data-role="fieldcontain"> <label for="name">Robot name is:</label> <textarea cols="24" name="name" id="name"></textarea> </div> <div data-role="fieldcontain"> <label for="host">Robot API host:</label> <textarea cols="24" name="host" id="host"></textarea> </div> <div data-role="fieldcontain"> <label for="port">Robot API port:</label> <textarea cols="24" name="port" id="port"></textarea> </div> <div data-role="fieldcontain"> <label for="device">Robot API device:</label> <textarea cols="24" name="device" id="device"></textarea> </div> <br/> <div class="ui-body ui-body-b"> <button type="submit" data-theme="a" id="b-submit">Submit</button> </div> </div> </div> <script> window.onload = function () { "use strict"; document.getElementById("name").value    = NAME; document.getElementById("host").value    = HOST; document.getElementById("port").value    = PORT; document.getElementById("device").value  = DEVICE;}; function saveOptions() { "use strict"; var options = { "name":    document.getElementById("name").value, "host":    document.getElementById("host").value, "port":    document.getElementById("port").value, "device":  document.getElementById("device").value }; return options; } document.getElementById("b-submit").addEventListener("click", function () { "use strict"; console.log("Submit"); var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(saveOptions())); console.log("Warping to: " + location); console.log(location); document.location = location; }); </script> </body> </html><!--.html';
 
-  var html = '<html> <head> <title>watchbot configuration</title> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> </head> <style> body, textarea, button { font-size: 20px; font-family: monospace; text-align: center; } textarea { resize: none; padding-top: 15px; } button { padding: 15px; } </style> <body> <div data-role="page" id="main"> <div data-role="header" class="jqm-header"> <h1>Configure watchbot</h1> </div> <div data-role="content"> <div data-role="fieldcontain"> <label for="name">Robot name is:</label> <textarea cols="24" name="name" id="name"></textarea> </div> <div data-role="fieldcontain"> <label for="host">Robot API host:</label> <textarea cols="24" name="host" id="host"></textarea> </div> <div data-role="fieldcontain"> <label for="port">Robot API port:</label> <textarea cols="24" name="port" id="port"></textarea> </div> <div data-role="fieldcontain"> <label for="device">Robot API device:</label> <textarea cols="24" name="device" id="device"></textarea> </div> <div data-role="fieldcontain"> <label for="accelerometer">Accelerometer events:</label> <input type="checkbox" name="accelerometer" id="accelerometer" /> </div> <br/> <div class="ui-body ui-body-b"> <button type="submit" data-theme="a" id="b-submit">Submit</button> <button type="submit" data-theme="d" id="b-cancel">Cancel</button> </div> </div> </div> <script> window.onload = function () { "use strict"; document.getElementById("name").value    = NAME; document.getElementById("host").value    = HOST; document.getElementById("port").value    = PORT; document.getElementById("device").value  = DEVICE; document.getElementById("accelerometer").checked = ACCELEROMETER}; function saveOptions() { "use strict"; var options = { "name":    document.getElementById("name").value, "host":    document.getElementById("host").value, "port":    document.getElementById("port").value, "device":  document.getElementById("device").value, "accelerometer": document.getElementById("accelerometer").checked }; return options; } document.getElementById("b-cancel").addEventListener("click", function () { "use strict"; console.log("Cancel"); document.location = "pebblejs://close"; }); document.getElementById("b-submit").addEventListener("click", function () { "use strict"; console.log("Submit"); var location = "pebblejs://close#" + encodeURIComponent(JSON.stringify(saveOptions())); console.log("Warping to: " + location); console.log(location); document.location = location; }); </script> </body> </html><!--.html';
-
-  html = html.replace("NAME", '"' + (localStorage.name || "pebble") + '"');
-  html = html.replace("HOST", '"' + (localStorage.host || "http://0.0.0.0") + '"');
-  html = html.replace("PORT", '"' + (localStorage.port || "8080") + '"');
-  html = html.replace("DEVICE", '"' + (localStorage.device || "pebble") + '"');
-  html = html.replace("ACCELEROMETER", (localStorage.accelerometer == "true"));
+  html = html.replace("NAME", '"' + (Settings.data('name') || "pebble") + '"');
+  html = html.replace("HOST", '"' + (Settings.data('host') || "http://0.0.0.0") + '"');
+  html = html.replace("PORT", '"' + (Settings.data('port') || "8080") + '"');
+  html = html.replace("DEVICE", '"' + (Settings.data('device') || "pebble") + '"');
 
   Pebble.openURL('data:text/html,' + encodeURI(html));
 });
@@ -64,14 +63,14 @@ Pebble.addEventListener("showConfiguration", function() {
 Pebble.addEventListener("webviewclosed", function(e) {
   var options = JSON.parse(decodeURIComponent(e.response));
 
-  localStorage.host = options.host;
-  localStorage.name = options.name;
-  localStorage.port = options.port;
-  localStorage.device = options.device;
+  Settings.data('host', options.host);
+  Settings.data('name', options.name);
+  Settings.data('port', options.port);
+  Settings.data('device', options.device);
 });
 
 Accel.init();
-Accel.config({rate: 25, samples: 1});
+Accel.config({rate: 10, samples: 1});
 
 var main = new UI.Menu({
   sections: [{
